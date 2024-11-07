babe34ee13bed588c73d3a0b017607bd56f75548
Vaishnavi Jangam <jangamvaishnavi090303.vpj@gmail.com>
2024-11-08 00:17:52 +0530

    Update Bank.sol

--- BCT/Bank.sol
+++ BCT/Bank.sol
@@ -1,31 +1,30 @@
 //SPDX-License-Identifier: UNLICENSED
 
 pragma solidity ^0.8.0;
-
-contract Bank{
-
+contract BankAcc{
     address public accHolder;
+    uint256 Balance=0;
 
-constructor() {
+    constructor(){
+        accHolder=msg.sender;
+    }
 
-    accHolder = msg.sender;
-}
+    function Withdraw() public payable {
+        require(Balance > 0, "You don't have enough balance");
+        require(msg.sender == accHolder, "You are not Account Holder");
+        payable( msg.sender).transfer(Balance);
+        Balance=0;
 
-uint256 balance=0;
+    }
+    function Deposite() public payable{
+        require(msg.sender == accHolder, "You are not Account Holder");
+        require(msg.value > 0, "Deposite amount should be grater than 0");
+        Balance+=msg.value;
 
-function withdraw() public payable {
-       require(msg.value > 0, "Withdraw am,out should be greater  than 0");
-       require(msg.sender == accHolder, "You are not acc owner");
-       payable(msg.sender).transfer(balance);
-       balance = 0;
-}
+    }
+    function showBalance() view public returns(uint){
+        require(msg.sender == accHolder, "You are not Account Holder");
+        return Balance;
+    }
 
-function deposite() public payable {
-    require(msg.value > 0, "You are not acc owner");
-    balance+= msg.value;
-}
-
-function showBalance() view public returns(uint){
-    return balance;
-}
 }
